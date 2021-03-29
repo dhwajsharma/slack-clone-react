a0af8f69f6b88af22809246b480392d158fe97df
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-03-29 12:14:34 +0530

    Building Message container

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,18 +3,31 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import styled from "styled-components";
 import { selectRoomId } from "../../features/appSlice"
+import { db } from '../../firebase';
 import ChatInput from '../ChatInput/ChatInput';
+import { useCollection, useDocument } from "react-firebase-hooks/firestore"
+import Message from '../Message/Message';
 
 const Chat = () => {
 
     const roomId = useSelector(selectRoomId);
+    const [roomDetails] = useDocument(
+        roomId && db.collection("rooms").doc(roomId)
+    )
+
+    const [roomMessages] = useCollection(
+        roomId &&
+        db.collection("rooms").doc(roomId).collection("messages").orderBy("timestamp", "asc")
+    )
+
+
 
     return (
         <ChatContainer>
             <>
                 <Header>
                     <HeaderLeft>
-                        <h4><strong>#Room-name</strong></h4>
+                        <h4><strong>#{roomDetails?.data().name}</strong></h4>
                         <StarBorderOutlined />
                     </HeaderLeft>
                     <HeaderRight>
@@ -24,9 +37,20 @@ const Chat = () => {
                     </HeaderRight>
                 </Header>
                 <ChatMessages>
-                    {/* List out the messages */}
+                    {roomMessages?.docs.map(doc => {
+                        const { message, timestamp, user, userImage } = doc.data()
+                        return (
+                            <Message
+                                key={doc.id}
+                                message={message}
+                                timestamp={timestamp}
+                                user={user}
+                                userImage={userImage}
+                            />
+                        )
+                    })}
                 </ChatMessages>
-                <ChatInput channelId={roomId} />
+                <ChatInput channelName={roomDetails?.data().name} channelId={roomId} />
             </>
         </ChatContainer>
     )
--- src/components/ChatInput/ChatInput.js
+++ src/components/ChatInput/ChatInput.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import { Button } from '@material-ui/core'
+import React, { useRef, useState } from 'react'
+import styled from 'styled-components'
+import { db } from '../../firebase';
+import firebase from 'firebase'
 
-const ChatInput = () => {
+const ChatInput = ({ channelName, channelId }) => {
+    const [input, setInput] = useState("")
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        if (!channelId) {
+            return false;
+        }
+
+        db.collection("rooms").doc(channelId).collection("messages").add({
+            message: input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            user: "Dhwaj Sharma",
+            userImage: "https://res.cloudinary.com/practicaldev/image/fetch/s--iRpSp4yU--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/314817/2231b1c6-4745-4899-9697-33ddf8fb8ea6.jpg"
+        });
+
+        setInput("");
+
+    };
+
     return (
-        <div>
-
-        </div>
+        <ChatInputContainer>
+            <form>
+                <input
+                    value={input}
+                    onChange={e => setInput(e.target.value)}
+                    placeholder={`Message #${channelName}`}
+                />
+                <Button hidden type="submit" onClick={sendMessage}>
+                    SEND
+                </Button>
+            </form>
+        </ChatInputContainer>
     )
 }
 
 export default ChatInput
+
+const ChatInputContainer = styled.div`
+    border-radius: 20px;
+
+    >form{
+        position: relative;
+        display: flex;
+        justify-content:center;
+    }
+
+    >form>input{
+        position: fixed;
+        bottom: 30px;
+        width: 60%;
+        border: 1px solid gray;
+        border-radius: 3px;
+        padding: 20px;
+        outline: none;
+    }
+
+    >form> button{
+        display: none !important;
+    }
+`
